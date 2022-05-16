66c14ac55a3da0bab48cbaa4f6e3015011701f69
王世翰 <60683892+dimpDEV@users.noreply.github.com>
2022-05-16 22:07:15 +0700

    first deploy

---
+++ src/Data/Connect.java
@@ -0,0 +1,35 @@
+/*
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
+package Data;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connect {
+    private static Connection con;
+
+    public static Connection getConnect() {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            con = DriverManager.getConnection(
+                    "jdbc:sqlserver://bfdyllagpx9xmspozzom-mysql.services.clever-cloud.com;databaseName=bfdyllagpx9xmspozzom;Username=uihxw415jvbmmula;Password = yen6f9KlpbvG7Pr7Zcy2");
+
+        } catch (Exception e) {
+            System.out.println("");
+        }
+        return con;
+    }
+
+    public static String testConnect() {
+        try {
+            con = Connect.getConnect();
+            return "";
+        } catch (Exception e) {
+            return "";
+        }
+    }
+}
