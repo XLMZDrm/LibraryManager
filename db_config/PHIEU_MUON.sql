c145190d8f38bc63af05aee99b8520d9fcf5af46
王世翰 <60683892+dimpDEV@users.noreply.github.com>
2022-05-18 10:54:12 +0700

    fix db

--- db_config/PHIEU_MUON.sql
+++ db_config/PHIEU_MUON.sql
@@ -1,11 +1,11 @@
 CREATE TABLE PHIEU_MUON(
-`Ma_Phieu_muon` varchar(10) DEFAULT NULL,
-  `Ma_Khach_hang` varchar(10) DEFAULT NULL,
-  `Ma_Sach` varchar(10) DEFAULT NULL,
-  `Ngay_muon` varchar(10) DEFAULT NULL,
-  `Han_tra` varchar(10) DEFAULT NULL,
-  `Ngaytra` varchar(10) DEFAULT NULL,
+    `Ma_Phieu_muon` varchar(10) DEFAULT NULL,
+    `Ma_Khach_hang` varchar(10) DEFAULT NULL,
+    `Ma_Sach` varchar(10) DEFAULT NULL,
+    `Ngay_muon` varchar(10) DEFAULT NULL,
+    `Han_tra` varchar(10) DEFAULT NULL,
+    `Ngaytra` varchar(10) DEFAULT NULL,
     PRIMARY KEY (Ma_Phieu_muon),
     FOREIGN KEY (Ma_Khach_hang) REFERENCES KHACH_HANG (Ma_Khach_hang),
     FOREIGN KEY (Ma_Sach) REFERENCES SACH (Ma_Sach)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4
--- db_config/SACH.sql
+++ db_config/SACH.sql
@@ -2,7 +2,7 @@ CREATE TABLE SACH(
     `Ma_Sach` varchar(10) DEFAULT NULL,
     `Ten_Sach` varchar(50) DEFAULT NULL,
     `Ten_Tac_gia` varchar(50) DEFAULT NULL,
-    `Nha_xb` varchar(11) DEFAULT NULL,
+    `Nha_xb` varchar(50) DEFAULT NULL,
     `Gia_tien` mediumint(9) DEFAULT NULL,
     `So_luong` tinyint(4) DEFAULT NULL,
     PRIMARY KEY (Ma_Sach)
