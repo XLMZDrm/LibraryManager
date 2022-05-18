5fa76261527c68aa85bba1820490af4354ed2d86
王世翰 <60683892+dimpDEV@users.noreply.github.com>
2022-05-18 10:52:26 +0700

    fix db

--- db_config/KHACH_HANG.sql
+++ db_config/KHACH_HANG.sql
@@ -1,9 +1,9 @@
 CREATE TABLE KHACH_HANG(
     `Ma_Khach_hang` varchar(10) DEFAULT NULL,
-    `Password` mediumint(9) DEFAULT NULL,
+    `Password` varchar(50) DEFAULT NULL,
     `Ten_Khach_hang` varchar(50) DEFAULT NULL,
     `Ngay_sinh` varchar(10) DEFAULT NULL,
-    `Dia_Chi` varchar(10) DEFAULT NULL,
-    `Phone` bigint(20) DEFAULT NULL,
+    `Dia_Chi` text DEFAULT NULL,
+    `Phone` char(10) DEFAULT NULL,
     PRIMARY KEY (Ma_Khach_hang)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4
